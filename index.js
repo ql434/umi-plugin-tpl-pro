1b7f695a70a404c50ac44a5de846b83d2892e177
ql434 <kunqian434@gmail.com>
2018-11-26 17:36:48 +0800

    feat: 工程化

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 /**
  * Copyright(c) Alibaba Group Holding Limited.
  *
- * @file:     为pro定制业务模板
+ * @file:     入口文件
  * @authors:  qiankun <chuck.ql@alibaba-inc.com> (https://work.alibaba-inc.com/work/u/85053)
- * @date      18/11/8
+ * @date      18/11/26
  */
-import { readFileSync } from 'fs';
-import { join, dirname, basename, extname } from 'path';
-import globby from 'globby';
-import uniq from 'lodash.uniq';
-import isRoot from 'path-is-root';
-import { chunkName, findJS, optsToArray, endWithSlash } from 'umi-utils';
 
-export default function (api, opts = {}) {
-  const { paths, cwd, compatDirname, winPath } = api;
-  const isDev = process.env.NODE_ENV === 'development';
-  const isProduction = process.env.NODE_ENV === 'production';
 
-  // 监听插件配置变化
-  api.onOptionChange((newOpts) => {
-    opts = newOpts;
-    api.rebuildTmpFiles();
-  });
-
-  function getModelName(model) {
-    const modelArr = winPath(model).split('/');
-    return modelArr[modelArr.length - 1];
-  }
-
-  api.registerGenerator('pro:table', {
-    Generator: require('./model').default(api),
-    resolved: join(__dirname, './model'),
-  });
-}
+'use strict';
+module.exports = require('./src/index.js');
--- src/model.js
+++ src/model.js
@@ -1,38 +1,63 @@
-/**
- * Copyright(c) Alibaba Group Holding Limited.
- *
- * @file:     写入model
- * @authors:  qiankun <chuck.ql@alibaba-inc.com> (https://work.alibaba-inc.com/work/u/85053)
- * @date      18/11/26
- */
+'use strict';
 
-import { join } from 'path';
-import assert from 'assert';
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-export default api => {
-  const { paths, config } = api;
-  const absTemplatePath = join(__dirname, '../template/simpleTable');
-  console.log(absTemplatePath)
-  return class Generator extends api.Generator {
-    writing() {
-      if (config.routes) {
-        throw new Error(`umi g page does not work when config.routes exists`);
-      }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-      const models = config.singular ? 'model' : 'models';
-      const name = this.args[0].toString();
-      assert(
-        !name.includes('/'),
-        `model name should not contains /, bug got ${name}`,
-      );
+var _path = require('path');
 
-      this.fs.copyTpl(
-        join(absTemplatePath, 'model.js'),
-        join(paths.absSrcPath, 'model', `${name}.js`),
-        {
-          name,
-        },
-      );
+var _assert = require('assert');
+
+var _assert2 = _interopRequireDefault(_assert);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * Copyright(c) Alibaba Group Holding Limited.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                *
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * @file:     写入model
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * @authors:  qiankun <chuck.ql@alibaba-inc.com> (https://work.alibaba-inc.com/work/u/85053)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * @date      18/11/26
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                */
+
+exports.default = function (api) {
+  var paths = api.paths,
+      config = api.config;
+
+  var absTemplatePath = (0, _path.join)(__dirname, '../template/simpleTable');
+  console.log(absTemplatePath);
+  return function (_api$Generator) {
+    _inherits(Generator, _api$Generator);
+
+    function Generator() {
+      _classCallCheck(this, Generator);
+
+      return _possibleConstructorReturn(this, (Generator.__proto__ || Object.getPrototypeOf(Generator)).apply(this, arguments));
     }
-  };
+
+    _createClass(Generator, [{
+      key: 'writing',
+      value: function writing() {
+        if (config.routes) {
+          throw new Error('umi g page does not work when config.routes exists');
+        }
+
+        var models = config.singular ? 'model' : 'models';
+        var name = this.args[0].toString();
+        (0, _assert2.default)(!name.includes('/'), 'model name should not contains /, bug got ' + name);
+
+        this.fs.copyTpl((0, _path.join)(absTemplatePath, 'model.js'), (0, _path.join)(paths.absSrcPath, 'model', name + '.js'), {
+          name: name
+        });
+      }
+    }]);
+
+    return Generator;
+  }(api.Generator);
 };
